4d2d913b12b6ab0ef048837b0555183b409f4cb6
Concepción <og17007@ues.edu.sv>
2021-01-14 00:01:13 -0600

    server caído

--- src/index.js
+++ src/index.js
@@ -28,3 +28,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 server.listen(3000, () => {
     console.log('Server on port 3000');
 });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
